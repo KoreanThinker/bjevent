3d8d0087afacb1c682c6d5de217b4756365bbce2
hyun <coderhyun476@gmail.com>
2020-03-03 17:40:00 +0900

    3학년 반배정 추가

--- web/src/shared/App.js
+++ web/src/shared/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, Event1, Event2, EventList, EventClose } from 'pages';
 import { fire } from './Firebase';
+import class3 from '../pages/class3';
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class App extends Component {
           <Route exact path="/event2" component={Event2} />
           <Route exact path="/eventlist" component={EventList} />
           <Route exact path="/eventClose" component={EventClose} />
+          <Route exact path='/class3' component={class3} />
           {/* <Route path="/:name" component={event} /> */}
         </Switch>
       </div>
